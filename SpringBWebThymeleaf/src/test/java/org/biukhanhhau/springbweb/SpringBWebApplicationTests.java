58c5eae74796da81b5da49370b8c49ecbb516c02
biukhanhhau <biu12.02dev@gmail.com>
2025-11-17 22:36:22 -0500

    for better way SpringBWeb

---
+++ SpringBWebThymeleaf/src/test/java/org/biukhanhhau/springbweb/SpringBWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.biukhanhhau.springbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
